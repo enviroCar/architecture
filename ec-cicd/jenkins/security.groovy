2e6098c16a0ef17c5c3c474911c523108bef9abf
Wall, Arne de (52-North) <a.dewall@52north.org>
2019-04-24 16:23:42 +0200

    compose files for ci/cd (docker-based)

---
+++ ec-cicd/jenkins/security.groovy
@@ -0,0 +1,18 @@
+#!groovy
+
+import jenkins.model.*
+import hudson.security.*
+import jenkins.security.s2m.AdminWhitelistRule
+
+def jenkins = Jenkins.getInstance()
+
+def username = System.getenv('JENKINS_USERNAME').trim()
+def password = System.getenv('JENKINS_PASSWORD').trim()
+
+def realm = new HudsonPrivateSecurityRealm(false)
+realm.createAccount(username, password)
+jenkins.setSecurityRealm(realm)
+jenkins.setAuthorizationStrategy(new FullControlOnceLoggedInAuthorizationStrategy())
+jenkins.save()
+
+jenkins.getInjector().getInstance(AdminWhitelistRule.class).setMasterKillSwitch(false)
