56f317f58f9b060b6b2f628afc505bc5f59802c8
Christian Autermann <c.autermann@52north.org>
2019-04-29 11:57:43 +0200

    Update jobs.groovy

--- ec-cicd/jenkins/jobs.groovy
+++ ec-cicd/jenkins/jobs.groovy
@@ -3,13 +3,12 @@
 import jenkins.model.Jenkins
 import groovy.io.FileType
 
-def jobDirectory = '/usr/share/jenkins/jobs'
 def jenkins = Jenkins.getInstance()
 
-new File(jobDirectory).eachFileRecurse(FileType.FILES) { file ->
+new File('/usr/share/jenkins/jobs').eachFileRecurse(FileType.FILES) { file ->
   if (file.name.endsWith(".job.xml")) {
     def jobName = file.name[0..-9]
-    if (jenkins.instance.getItem(jobName) == null) {
+    if (jenkins.getItem(jobName) == null) {
       file.withInputStream { stream ->
         jenkins.createProjectFromXML(jobName, stream)
       }
